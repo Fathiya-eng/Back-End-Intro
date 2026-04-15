94dee673a336b5643ce1dd96ee417067d3b6698b
Fathiya Al-Fahdi <fathiyaalfahdi@gmail.com>
2026-04-15 08:20:40 +0400

    Add files via upload

---
+++ Solution03/Project-Solution01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Project_Solution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine("Please Enter First number!");
+            
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Second number!");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        }
+    }
+}
