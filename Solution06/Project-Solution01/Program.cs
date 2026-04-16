27a8cf4c8427da31805720ff6e5724e37a283e9c
Fathiya Al-Fahdi <fathiyaalfahdi@gmail.com>
2026-04-16 12:31:31 +0400

    Add files via upload
    practice of looping

---
+++ Solution06/Project-Solution01/Program.cs
@@ -0,0 +1,139 @@
+﻿namespace Project_Solution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region for loop-1
+            //for (int x = 1; x < 5; x++)
+
+            //{
+            //    Console.WriteLine("Enter a number: ");
+
+            //    int user = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine($"The number you Enter is: {x}");
+            //} 
+            #endregion
+
+            //=============================================================
+
+
+            #region for loop-2
+            //for (int x = 1; x <= 5; x++)
+            //{
+            //    for (int i= 1; i <= 5; i++)
+            //    {
+            //        Console.WriteLine($"({x} , {i})");
+            //    }
+            //} 
+            #endregion
+
+            //=============================================================
+
+            #region prime number
+            //Console.WriteLine("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //bool koob = true;
+
+            //if (number <= 1)
+            //{
+            //    koob = false;
+            //}
+            //else
+            //{
+            //    for (int i = 2; i < number; i++)
+            //    {
+            //        if (number % i == 0)
+            //        {
+            //            koob = false;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //if (koob)
+            //{
+            //    Console.WriteLine("The number you entered is prime");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number you entered is NOT prime");
+            //} 
+            #endregion
+
+
+            //===============================================================
+
+            #region while loop 
+
+            //Console.WriteLine("Please Enter any Number:");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+
+            //while (num!=5)
+            //{
+            //    Console.WriteLine("Incorrect Number:");
+            //    num = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            #endregion
+
+
+            //===============================================================
+
+            #region Random number-1
+
+            //Console.WriteLine(" the random number is from 1 to 5");
+
+            //Random rnd = new Random();
+            //int num1 = rnd.Next(1, 6);
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //while (num != num1)
+            //{
+            //    Console.WriteLine("Incorrect Number");
+            //    num = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Correct");
+            #endregion
+
+
+            //===============================================================
+
+            #region Random number-2  
+
+            //Random rnd = new Random();
+            //int count = 0;
+
+            //while (count < 3)
+            //{
+
+            //    int randomNumber = rnd.Next(1, 10);
+
+            //    Console.WriteLine($"Random Number {count + 1}: {randomNumber}");
+            //    count++;
+            //}
+
+            #endregion
+
+
+
+            //===============================================================
+            #region MyRegion
+
+
+            #endregion
+
+
+
+            //===============================================================
+
+        }
+    }
+}
