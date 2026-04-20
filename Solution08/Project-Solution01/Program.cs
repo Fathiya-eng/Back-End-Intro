097c637be19257862e00c5e1166b69a0c28217a7
Fathiya Al-Fahdi <fathiyaalfahdi@gmail.com>
2026-04-20 11:53:32 +0400

    Add Solution08
    Array Tasks

---
+++ Solution08/Project-Solution01/Program.cs
@@ -0,0 +1,224 @@
+﻿namespace Project_Solution01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Array_Practice
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine("Enter any numbers: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine(Array.IndexOf(numbers,10));
+            //Array.Sort(numbers);
+
+            #endregion
+
+            //===========================================================
+
+            #region Task1 
+            //Task1:
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine(numbers[i]);
+            //} 
+            #endregion
+
+            //============================================================
+
+            #region Task2 
+            //Task2:
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            #endregion
+
+            //============================================================
+
+            #region Task3 
+            //Task3:
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine($"Enter any 5 numbers {i + 1}: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("Result:");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //} 
+            #endregion
+
+            //============================================================
+
+            #region Task4 
+            //Task4:
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine($"Enter any 5 numbers {i+1}: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("Sum Of Numbers= " + numbers.Sum()); 
+            #endregion
+
+            //============================================================
+
+            #region Task5 
+            //Task5:
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine($"Enter any 5 numbers {i + 1}:");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("Max= " + numbers.Max()); 
+            #endregion
+
+            //============================================================
+
+            #region Task6 
+            //Task6: 
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine($"Enter any 5 numbers {i + 1}:");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("Min= " + numbers.Min());
+
+            #endregion
+           
+            //============================================================
+
+            #region Task7 
+            //Task7:
+
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter any 10 numbers {i+1}: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("===========================");
+            //Console.WriteLine("Even numbers are:");
+
+            //foreach (int num in numbers)
+            //{
+            //    if (num % 2 == 0)
+            //    {
+            //        Console.WriteLine(num);
+            //    }
+            //} 
+            #endregion
+
+            //===========================================================
+
+            #region Task8 
+            //Task8:
+
+            //int[] numbers = new int[5];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    Console.WriteLine($"Enter any 5 numbers {i + 1}: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("Average Of Numbers= " + numbers.Average()); 
+            #endregion
+
+            //============================================================
+
+            #region Task9 
+            //Task9:
+
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine("Before reversing:");
+            //foreach (int num in numbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //Console.WriteLine("===========================");
+
+            //Console.WriteLine("After reversing:");
+            //for (int i = numbers.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //} 
+            #endregion
+
+            //============================================================
+
+            #region Task10 
+            //Task10:
+
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine("Enter number to search:");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //bool found = false;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] == num)
+            //    {
+            //        Console.WriteLine($"Number found at index {i}");
+            //        found = true;
+            //        break;
+            //    }
+            //}
+
+            //if (!found)
+            //{
+            //    Console.WriteLine("Number not found");
+            //} 
+            #endregion
+
+            //============================================================
+        }
+    }
+}
