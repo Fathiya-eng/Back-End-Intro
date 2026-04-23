21e9422ba1a5ce188004c5115b90d85aca82dd64
Fathiya Al-Fahdi <fathiyaalfahdi@gmail.com>
2026-04-23 12:37:11 +0400

    Student Management System using Windows Forms
    First Task for Windows Forms

---
+++ StudentManagement01/StudentManagement/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace StudentManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.StudentName = new System.Windows.Forms.TextBox();
+            this.StudentAge = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(79, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(603, 541);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // StudentName
+            // 
+            this.StudentName.Location = new System.Drawing.Point(225, 385);
+            this.StudentName.Name = "StudentName";
+            this.StudentName.Size = new System.Drawing.Size(159, 20);
+            this.StudentName.TabIndex = 1;
+            this.StudentName.Text = "Name";
+            this.StudentName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // StudentAge
+            // 
+            this.StudentAge.Location = new System.Drawing.Point(225, 424);
+            this.StudentAge.Name = "StudentAge";
+            this.StudentAge.Size = new System.Drawing.Size(159, 20);
+            this.StudentAge.TabIndex = 2;
+            this.StudentAge.Text = "Age";
+            this.StudentAge.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(563, 385);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(102, 20);
+            this.Search.TabIndex = 3;
+            this.Search.Text = "Search";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 496);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(563, 417);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(309, 496);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Show All";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter Student Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter Student Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(434, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Search For a Student:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 657);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.StudentAge);
+            this.Controls.Add(this.StudentName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox StudentName;
+        private System.Windows.Forms.TextBox StudentAge;
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StudentManagement01/StudentManagement/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace StudentManagement
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        int currentId = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void RefreshGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = students;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(StudentName.Text))
+            {
+                MessageBox.Show("Enter student name");
+                return;
+            }
+
+            int age;
+
+            if (!int.TryParse(StudentAge.Text, out age))
+            {
+                MessageBox.Show("Enter valid age");
+                return;
+            }
+
+            Student s = new Student()
+            {
+                ID = currentId++,
+                Name = StudentName.Text,
+                Age = age
+            };
+
+            students.Add(s);
+            RefreshGrid();
+            StudentName.Clear();
+            StudentAge.Clear();
+            StudentName.Focus();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                var confirm = MessageBox.Show("Are you sure?", "Delete", MessageBoxButtons.YesNo);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    Student selected = (Student)dataGridView1.CurrentRow.DataBoundItem;
+                    students.Remove(selected);
+                    RefreshGrid();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result = students
+    .Where(s => s.Name.Contains(Search.Text))
+    .ToList();
+
+            if (result.Count == 0)
+            {
+                MessageBox.Show("Student not found");
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+    }
+}
