98e2cb16eb98608942a6d39e1b6a220020d19e78
Fathiya Al-Fahdi <fathiyaalfahdi@gmail.com>
2026-05-10 13:50:31 +0400

    Abstract and Interface

---
+++ Solution20/Project-Solution01/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Project_Solution01
+{
+    internal class Program
+    {
+        #region Abstract: Animals
+        //abstract class Animal()
+        //{
+        //    public abstract void sound();
+        //    public abstract void Eat();
+        //}
+
+        //class Dog : Animal
+        //{
+        //    public override void sound()
+        //    {
+        //        Console.WriteLine("Barking");
+        //    }
+        //    public override void Eat()
+        //    {
+        //        Console.WriteLine("Meat");
+        //    }
+
+        //} 
+        #endregion
+
+        //======================================
+
+        #region Abstract BankAcount
+        //abstract class BankAcount()
+        //{
+        //    public abstract void Withdraw();
+
+        //}
+
+        //class Saving : BankAcount
+        //{
+        //    public override void Withdraw()
+        //    {
+        //        Console.WriteLine("Saving");
+        //    }
+
+        //}
+
+        //class Current : BankAcount
+        //{
+        //    public override void Withdraw()
+        //    {
+        //        Console.WriteLine("Current");
+        //    }
+
+        //} 
+        #endregion
+
+        //=======================================
+
+        #region Interface
+        interface Bird
+        {
+            string Name { get; }
+            public void Fly();
+        }
+
+        class Hodhod : Bird
+        {
+            public string Name { get; set; }
+            public void Fly()
+            {
+                Console.WriteLine("Hello");
+            }
+        } 
+        #endregion
+
+        //=======================================
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
